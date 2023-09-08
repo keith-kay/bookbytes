9f7a64802eb39225859fa20b9cefa5b87974cc21
keith-kay <keithrhova@gmail.com>
2023-09-08 17:04:55 +0300

    update login and logout

--- bytes/urls.py
+++ bytes/urls.py
@@ -4,9 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('books', views.book_list, name='book_list'),
-    # path('',views.index, name='home'),
     path('base/', TemplateView.as_view(template_name='bytes/base.html'), name='base'),
     path('', TemplateView.as_view(template_name='bytes/index.html'), name='index'),
     path('signin/', TemplateView.as_view(template_name='bytes/signin.html'), name='signin'),
     path('signup/', TemplateView.as_view(template_name='bytes/signup.html'), name='signup'),
+    path('login/', views.loginPage, name="loginPage"),
+    path('logout/',views.logoutPage, name="logout")
 ]
--- bytes/views.py
+++ bytes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 import requests 
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -13,7 +14,26 @@ class SignUpView(TemplateView):
 class SignInView(TemplateView):
     template_name = 'signin.html'
 
+#create user login session
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)#pass in the request
+            return redirect('base')  # Redirect to the 'base' URL pattern
+    return render(request, 'signin')
+
+#logout user session
+def logoutPage(request):
+    logout(request)
+    return redirect('signin')
+            
+
+@login_required(login_url='signin')
 def book_list(request):
     # API URL for fetching book data (replace with actual API endpoint)
     api_url = "https://www.googleapis.com/books/v1/volumes?q=python"
@@ -33,6 +53,7 @@ def book_list(request):
 
     return render(request, 'bytes/book_list.html', {'books': books})
 
+@login_required(login_url='signin')
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
